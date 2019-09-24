34d80d07098218b81cc7669b8810eb9204a5359d
Martin Czygan <martin.czygan@gmail.com>
2019-09-24 18:40:59 +0200

    fix

--- topics/language/slices/example8/example8.go
+++ topics/language/slices/example8/example8.go
@@ -11,14 +11,12 @@ func main() {
 	// Using the value semantic form of the for range.
 	friends := []string{"Annie", "Betty", "Charley", "Doug", "Edward"}
 	for _, v := range friends {
-		friends = friends[:2]
 		fmt.Printf("v[%s]\n", v)
 	}
 
 	// Using the pointer semantic form of the for range.
 	friends = []string{"Annie", "Betty", "Charley", "Doug", "Edward"}
 	for i := range friends {
-		friends = friends[:2]
 		fmt.Printf("v[%s]\n", friends[i])
 	}
 }
