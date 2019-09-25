02ed1825d207227e5b99388417c41d25ecb496df
Martin Czygan <martin.czygan@gmail.com>
2019-09-25 21:34:13 +0200

    num goroutines

--- topics/concurrency/csp/example1/main.go
+++ topics/concurrency/csp/example1/main.go
@@ -10,4 +10,9 @@ func main() {
 	// NumCPU returns the number of logical
 	// CPUs usable by the current process.
 	fmt.Println(runtime.NumCPU())
+
+	// go func() {}()
+	fmt.Println(runtime.NumGoroutine())
+	// time.Sleep(1 * time.Second)
+
 }
